9e6658d237f9c2cf25eeb8ba08457f32b9b3aee3
Jens Schauder <jens.schauder@freenet.de>
2016-03-26 14:00:35 +0100

    basic styling

--- diagram.js
+++ diagram.js
@@ -3,20 +3,26 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-                                                         ['Day',  'yellow', 'green'],
-                                                         ['2016-03-15', 9,  2],
-                                                         ['2016-03-18', 10, 4],
-                                                         ['2016-03-20', 10, 5],
-                                                         ['2016-03-24', 10, 6],
-                                                         ['style', 'color: yellow', 'color: green']
-                                                     ]);
+    var data = new google.visualization.DataTable();
+    //['Day',  'yellow', 'green'],
+    data.addColumn('string', 'Day');
+    data.addColumn('number', 'Yellow');
+    data.addColumn({type: 'string', role: 'style'})
+    data.addColumn('number', 'Green');
+    data.addColumn({type: 'string', role: 'style'})
+    data.addRows([
+                     ['2016-03-15',  9, 'color: yellow; stroke-width: 0', 2, 'color: green; stroke-width: 0'],
+                     ['2016-03-18', 10, 'color: yellow; stroke-width: 0', 4, 'color: green; stroke-width: 0'],
+                     ['2016-03-20', 10, 'color: yellow; stroke-width: 0', 5, 'color: green; stroke-width: 0'],
+                     ['2016-03-24', 10, 'color: yellow; stroke-width: 0', 6, 'color: green; stroke-width: 0']
+    ]);
+
 
     var options = {
         title: 'Progress in Bouldering',
         vAxis: {title: 'Boulder Problems'},
         connectSteps: false,
-        isStacked: true
+        isStacked: false
     };
 
     var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
