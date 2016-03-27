297e6ebe3aea05fd9abc1fe45d81d22100e3ebee
Jens Schauder <jens.schauder@freenet.de>
2016-03-27 14:25:56 +0200

    refactored to create a clean structure for creating the diagram

--- diagram.js
+++ diagram.js
@@ -1,22 +1,30 @@
 
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(displayBoulderingData);
 
-function drawChart() {
+function loadData() {
+    return [
+        ['2016-03-15',  9, 'color: yellow; stroke-width: 0', 2, 'color: green; stroke-width: 0'],
+        ['2016-03-18', 10, 'color: yellow; stroke-width: 0', 4, 'color: green; stroke-width: 0'],
+        ['2016-03-20', 10, 'color: yellow; stroke-width: 0', 5, 'color: green; stroke-width: 0'],
+        ['2016-03-24', 10, 'color: yellow; stroke-width: 0', 6, 'color: green; stroke-width: 0']
+    ]
+}
+
+
+function transform(originalData) {
+    return originalData;
+}
+
+function createDiagram(rows) {
     var data = new google.visualization.DataTable();
-    //['Day',  'yellow', 'green'],
+
     data.addColumn('string', 'Day');
     data.addColumn('number', 'Yellow');
     data.addColumn({type: 'string', role: 'style'});
     data.addColumn('number', 'Green');
     data.addColumn({type: 'string', role: 'style'});
-    data.addRows([
-                     ['2016-03-15',  9, 'color: yellow; stroke-width: 0', 2, 'color: green; stroke-width: 0'],
-                     ['2016-03-18', 10, 'color: yellow; stroke-width: 0', 4, 'color: green; stroke-width: 0'],
-                     ['2016-03-20', 10, 'color: yellow; stroke-width: 0', 5, 'color: green; stroke-width: 0'],
-                     ['2016-03-24', 10, 'color: yellow; stroke-width: 0', 6, 'color: green; stroke-width: 0']
-    ]);
-
+    data.addRows(rows);
 
     var options = {
         title: 'Progress in Bouldering',
@@ -28,4 +36,12 @@ function drawChart() {
     var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
 
     chart.draw(data, options);
+}
+
+function displayBoulderingData() {
+
+    var originalData = loadData();
+    var dataInGoogleFormat = transform(originalData);
+    createDiagram(dataInGoogleFormat);
+
 }
