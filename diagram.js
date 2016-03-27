45925d76746640b43e82b44cf284d3b5fb2f5e04
Jens Schauder <jens.schauder@freenet.de>
2016-03-27 14:36:16 +0200

    transform input data in the google row format in some super simplistic way

--- diagram.js
+++ diagram.js
@@ -3,17 +3,48 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(displayBoulderingData);
 
 function loadData() {
-    return [
+    return [{
+        "day": "2016-03-18",
+        "yellow": -3,
+        "green": 4
+    },{
+        "day": "2016-03-20",
+        "yellow": -2,
+        "green": 5
+    },{
+        "day": "2016-03-24",
+        "yellow": -1,
+        "green": 7
+    },{
+        "day": "2016-03-26",
+        "yellow": -0,
+        "green": 7
+    }
+    ];
+    /*
+    [
         ['2016-03-15',  9, 'color: yellow; stroke-width: 0', 2, 'color: green; stroke-width: 0'],
         ['2016-03-18', 10, 'color: yellow; stroke-width: 0', 4, 'color: green; stroke-width: 0'],
         ['2016-03-20', 10, 'color: yellow; stroke-width: 0', 5, 'color: green; stroke-width: 0'],
         ['2016-03-24', 10, 'color: yellow; stroke-width: 0', 6, 'color: green; stroke-width: 0']
     ]
+    */
 }
 
 
 function transform(originalData) {
-    return originalData;
+    var rows = [];
+    for (var i = 0; i < originalData.length; i++) {
+        var inputRow = originalData[i];
+        var row = [
+            inputRow.day,
+            inputRow.yellow, 'color: yellow; stroke-width: 0',
+            inputRow.green, 'color: green; stroke-width: 0'
+        ];
+        rows.push(row);
+    }
+
+    return rows;
 }
 
 function createDiagram(rows) {
